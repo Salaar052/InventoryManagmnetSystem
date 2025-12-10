b31aca4c68adf626a06804ec7e3191007f7ac29d
Salaar052 <salaarasim345@gmail.com>
2025-12-10 12:32:24 +0500

    first commit

---
+++ frontenedInventarySystem/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+import {
+  Login, 
+  SignUp, 
+  ForgotPassword
+} from "./pages/AuthPages/index.js"
+import {
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider
+} from "react-router-dom"
+import {AllProducts} from "./pages/ProductPages/AllProducts.jsx"
+import {InventoryLayout} from "./pages/Layouts/RootLayout.jsx"
+import { Dashboard } from './pages/DashboardPage/Dashboard.jsx'
+import { ProductDetailsPage } from './pages/ProductPages/ProductDetails.jsx'
+import { AllCategories } from "./pages/CategoryPages/AllCategories.jsx"
+import { CategoryDetailsPage } from "./pages/CategoryPages/CategoryDetails.jsx"
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/'>
+        {/* Auth Routes - Outside RootLayout */}
+        <Route path='login' element={<Login/>} />
+        <Route path='signup' element={<SignUp/>} />
+        <Route path='forgetPassword' element={<ForgotPassword/>} />
+         
+        {/* Protected Routes - Inside RootLayout */}
+        <Route element={<InventoryLayout />}>
+          <Route index element={<AllProducts />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<AllProducts />} />
+          <Route path="products/:id" element={<ProductDetailsPage />} />
+          <Route path="categories" element={<AllCategories />} />
+          <Route path="categories/:id" element={<CategoryDetailsPage />} /> 
+          <Route path="orders" element={<div>Orders Page</div>} />
+          <Route path="suppliers" element={<div>Suppliers Page</div>} />
+          <Route path="users" element={<div>Users Page</div>} />
+          <Route path="profile" element={<div>Profile Page</div>} />
+        </Route>
+
+      </Route>
+    )
+  )
+  return(
+    <>
+    <RouterProvider router={router}/>
+    </>
+  )
+}
+export default App
